43a49762212b82b6c48d58092ff76a4bf7c5c2e5
matblanco <mateoblaurq@gmail.com>
2020-09-15 21:25:29 -0400

    RegisteredAndDeletedGraduatedv3

--- src/app/services/studentService.js
+++ src/app/services/studentService.js
@@ -17,6 +17,23 @@ const StudentService = {
         } catch (error) {
            throw error;
         }
+	},
+	createStudent: async (request, response) => {
+        try {
+            const result = await StudentModel.create(request.body);
+            return {message : "Student was created successfully.",result};
+        } catch (error) {
+           throw error;
+        }
+	},
+	deleteStudent: async (request, response) => {
+        try {
+            const result = await StudentModel.destroy(
+            {where:{id:request.params.id}});
+            return {message : "Student was deleted successfully.",result};
+        } catch (error) {
+           throw error;
+        }
 	}
 }
 
--- src/routes/students.js
+++ src/routes/students.js
@@ -17,6 +17,23 @@ router.put('/:id',async (request,response)=>{
     } catch (error) {
         console.log(error);
     }
+});
+router.post(routeNames.emptyUrl, async (request,response)=>{
+    try {
+        const result = await studentService.createStudent(request,response);
+        response.status(200).send(result);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.delete('/:id',async (request,response)=>{
+    try {
+        const result = await studentService.deleteStudent(request,response);
+        response.status(200).send(result);
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 module.exports = router;
