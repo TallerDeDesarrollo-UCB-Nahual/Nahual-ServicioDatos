34045cafc0445dea82da0ad3f270d6d835a09c9c
Carlos Terceros <carlosterceros11@gmail.com>
2020-09-17 14:00:17 -0400

    Fixed to get only one object when getting student by id

--- src/app/services/studentService.js
+++ src/app/services/studentService.js
@@ -2,12 +2,7 @@ const StudentModel = require('../models/student');
 const StudentDTO = require('../models/DTOs/studentDTO');
 const StudentService = {
 	findGraduatedById: async (studentId) => {
-		let graduate = await StudentModel.findAll({
-			where: {
-				id: studentId
-			}
-		});
-		graduate = graduate.map(x => x.dataValues);
+		let graduate = await StudentModel.findByPk(studentId)
 		return graduate;
 	},
 	findGraduatedStudents: async (request, response) => {
