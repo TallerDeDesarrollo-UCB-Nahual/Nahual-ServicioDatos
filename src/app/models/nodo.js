50668fff3fd89911bcf3bbea8154830d9225ef1d
Carlos Terceros <carlosterceros11@gmail.com>
2020-10-07 19:33:44 -0400

    Deleted unnecesary files

--- src/app/models/nodo.js
+++ src/app/models/nodo.js
@@ -1,6 +1,3 @@
-const { DataTypes } = require("sequelize/types");
-const { sequelize } = require(".");
-
 module.exports = (sequelize, DataTypes) => {
   const Nodo = sequelize.define('Nodo', {
     'id': {
