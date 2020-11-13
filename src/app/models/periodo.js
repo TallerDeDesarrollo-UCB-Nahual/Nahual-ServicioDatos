2ae1ecfe175dd071fcfd72ff03981a5c8808b490
IvySaskia <saskiaivy@outlook.com>
2020-11-12 23:22:21 -0400

    Agregado autoIncrement en id de periodo

--- src/app/models/periodo.js
+++ src/app/models/periodo.js
@@ -2,7 +2,8 @@ module.exports = (sequelize, DataTypes) => {
     const Periodo = sequelize.define('Periodo', {
       'id': {
         type: DataTypes.INTEGER,
-        primaryKey: true
+        primaryKey: true,
+        autoIncrement: true
       },
       'anio': DataTypes.INTEGER,
       'periodo': DataTypes.STRING,
