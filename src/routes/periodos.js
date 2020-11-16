82838358d63c82d61eeb2a3be8c2e5fb1f79ca17
laura Meneses <lmeneses58@gmail.com>
2020-11-16 10:57:33 -0400

    add end points

--- src/routes/periodos.js
+++ src/routes/periodos.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const nombresRutas = require('../resources/nombresRutas');
 const periodoService = require('../app/services/periodoService');
 const cursoService = require('../app/services/cursoService');
+const { request, response } = require('express');
 
 router.get(nombresRutas.emptyUrl, async(request, response) => {
     let parametros = request.query;
@@ -25,6 +26,14 @@ router.delete('/:id/cursos/:idCurso', async(request, response) => {
         const result = await cursoService.eliminarCursoEnPeriodo(request.params.id, request.params.idCurso);
         response.status(200).send(result);
     } catch (error) {
+        console.log(error);
+    }
+});
+
+router.delete('/:id', async(request, response) => {
+    try {
+        const result = await periodoService.eliminarPeriodo(request.params.id);
+    }catch (error) {
         console.log(error);
     }
 });
