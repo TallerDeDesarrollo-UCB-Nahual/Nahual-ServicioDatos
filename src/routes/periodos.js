86f12d7f68a9f4be6e15555f82d820cc1a08b226
Diego Orlando Mejia Salazar <diego.mejia@fundacion-jala.org>
2020-11-16 10:01:05 -0400

    buscar cursos por id del periodo

--- src/routes/periodos.js
+++ src/routes/periodos.js
@@ -3,11 +3,17 @@ const express = require('express');
 const router = express.Router();
 const nombresRutas = require('../resources/nombresRutas');
 const periodoService = require('../app/services/periodoService');
+const servicioCursos = require('../app/services/cursoService');
 
 router.get(nombresRutas.emptyUrl, async(request, response) => {
     let parametros = request.query;
     const todosLosPeriodos = await periodoService.encontrarTodosLosPeriodos(parametros);
     response.send(todosLosPeriodos)
+});
+
+router.get("/:id/cursos", async(request, response) => {
+    const cursos = await servicioCursos.encontrarCursosPorPeriodo({PeriodoId: request.params.id});
+    response.send(cursos)
 });
 
 router.post(nombresRutas.emptyUrl, async(request, response) => {
