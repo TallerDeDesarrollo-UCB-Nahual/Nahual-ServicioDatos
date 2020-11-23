34ef7984b46f9151763f108c57ca049067573d5f
Mauricio Quezada <mauriqb2@gmail.com>
2020-11-23 18:26:49 -0400

    Aniadiendo codigo de inscripcion

--- src/app/services/inscriptoService.js
+++ src/app/services/inscriptoService.js
@@ -25,6 +25,62 @@ const InscriptoService = {
         var codigo;
         for(const inscripto of inscriptos ){
             let estudiante = await Estudiante.findByPk(inscripto.estudianteId);
+            let estaInscripto = await Inscripto.findAll({
+                where: {
+                    estudianteId: inscripto.estudianteId,
+                    cursoId: inscripto.cursoId
+                }
+            });
+            if(estaInscripto.length == 0){
+                if(estudiante == null){
+                    resultado.push({ Operacion: "El estudiante con id " + inscripto.estudianteId + " no existe"});
+                    codigo = 400;
+                }
+                else{
+                    let curso = await Curso.findByPk(inscripto.cursoId);
+                    if(curso == null){
+                        resultado.push({ Operacion: "El curso con id " + inscripto.estudianteId + " no existe"});
+                        codigo = 400;
+                    }
+                    else{
+                        if(estudiante.estadoId == 1){
+                            estudianteActualizado = {
+                                "nodoId": curso.nodoId,
+                                "sedeId": curso.sedeId,
+                                "estadoId": 2
+                            }
+                            await Estudiante.update(estudianteActualizado, { where: { id: inscripto.estudianteId } });
+                            await Inscripto.create(inscripto);
+                            resultado.push({ Operacion: "Se registro correctamente"});
+                            codigo = 200;
+                        }
+                        else{
+                            resultado.push({ Operacion: "El estudiante no es un pre-inscripte"});
+                            codigo = 400;
+                        } 
+                    }
+                }
+            }
+            else{
+                resultado.push({ Operacion: "El estudiante ya se encuentra inscripto a un curso"});
+                codigo = 400;
+            }     
+        };  
+        return {message: resultado, result: codigo}; 
+    },
+
+    inscribir: async(request, response) => {
+        var inscripto = request.body;
+        var resultado = [];
+        var codigo;
+        let estudiante = await Estudiante.findByPk(inscripto.estudianteId);
+        let estaInscripto = await Inscripto.findAll({
+            where: {
+                estudianteId: inscripto.estudianteId,
+                cursoId: inscripto.cursoId
+            }
+        });
+        if(estaInscripto.length == 0){
             if(estudiante == null){
                 resultado.push({ Operacion: "El estudiante con id " + inscripto.estudianteId + " no existe"});
                 codigo = 400;
@@ -53,44 +109,11 @@ const InscriptoService = {
                     } 
                 }
             }
-                 
-        };  
-        return {message: resultado, result: codigo}; 
-    },
-
-    inscribir: async(request, response) => {
-        var inscripto = request.body;
-        var resultado = [];
-        var codigo;
-        let estudiante = await Estudiante.findByPk(inscripto.estudianteId);
-        if(estudiante == null){
-            resultado.push({ Operacion: "El estudiante con id " + inscripto.estudianteId + " no existe"});
-            codigo = 400;
         }
         else{
-            let curso = await Curso.findByPk(inscripto.cursoId);
-            if(curso == null){
-                resultado.push({ Operacion: "El curso con id " + inscripto.estudianteId + " no existe"});
-                codigo = 400;
-            }
-            else{
-                if(estudiante.estadoId == 1){
-                    estudianteActualizado = {
-                        "nodoId": curso.nodoId,
-                        "sedeId": curso.sedeId,
-                        "estadoId": 2
-                    }
-                    await Estudiante.update(estudianteActualizado, { where: { id: inscripto.estudianteId } });
-                    await Inscripto.create(inscripto);
-                    resultado.push({ Operacion: "Se registro correctamente"});
-                    codigo = 200;
-                }
-                else{
-                    resultado.push({ Operacion: "El estudiante no es un pre-inscripte"});
-                    codigo = 400;
-                } 
-            }
-        }
+            resultado.push({ Operacion: "El estudiante ya se encuentra inscripto a un curso"});
+            codigo = 400;
+        }    
         return {message: resultado, result: codigo}; 
     },
 
