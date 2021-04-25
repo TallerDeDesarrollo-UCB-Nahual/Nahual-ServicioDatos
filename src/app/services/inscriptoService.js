17dd8fcd590d0c237d28ceffbab9596615974f4a
AdRiAnPg7 <adriancracker@gmail.com>
2021-04-25 02:48:35 -0400

    Modificando inscripcion

--- src/app/services/inscriptoService.js
+++ src/app/services/inscriptoService.js
@@ -92,21 +92,14 @@ const InscriptoService = {
                     codigo = 400;
                 }
                 else{
-                    if(estudiante.estadoId == 1){
-                        estudianteActualizado = {
-                            "nodoId": curso.NodoId,
-                            "sedeId": curso.SedeId,
-                            "estadoId": 2
-                        }
-                        await Estudiante.update(estudianteActualizado, { where: { id: inscripto.estudianteId } });
-                        await Inscripto.create(inscripto);
-                        resultado.push({ Operacion: "Se registro correctamente"});
-                        codigo = 200;
+                    estudianteActualizado = {
+                        "nodoId": curso.NodoId,
+                        "sedeId": curso.SedeId,
                     }
-                    else{
-                        resultado.push({ Operacion: "El estudiante no es un pre-inscripte"});
-                        codigo = 400;
-                    } 
+                    await Estudiante.update(estudianteActualizado, { where: { id: inscripto.estudianteId } });
+                    await Inscripto.create(inscripto);
+                    resultado.push({ Operacion: "Se registro correctamente"});
+                    codigo = 200;
                 }
             }
         }
